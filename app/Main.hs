14fe668ca1316a343c08eaf2b97c27fa8f6c96c6
Lyndon Maydwell <lyndon@sordina.net>
2016-09-07 15:12:15 +1000

    working reverse

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,63 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
 import Data.Maybe
+import Data.Tuple
 import Data.List.Split
+import Options.Generic
+import Prelude hiding (reverse)
 
 data Ending = Unix | DOS | Mac deriving (Eq, Ord, Show)
 
-main :: IO ()
-main = interact extract >> putStrLn ""
+data Options = Options { reverse :: Bool } deriving (Eq, Ord, Show, Generic)
 
-extract :: String -> String
-extract = mapMaybe fuck . chunksOf 2 . lexer
+instance ParseRecord Options
+
+main :: IO ()
+main = do
+  o <- getRecord "" :: IO Options
+  if (reverse o) then interact bf2le
+                 else interact le2bf
+  putStrLn "" -- Could make things non-isomorphic, but looks better and shouldn't be a big problem due to pairs behaviour.
+
+le2bf :: String -> String
+le2bf = mapMaybe fuck . chunksOf 2 . lexer
+
+bf2le :: String -> String
+bf2le = concatMap unfuck
 
 lexer :: String -> [Ending]
 lexer []                = []
 lexer ('\r' : '\n' : t) = DOS    : lexer t
-lexer ('\r' :        t) = Mac    : lexer t 
-lexer ('\n' :        t) = Unix   : lexer t 
+lexer ('\r' :        t) = Mac    : lexer t
+lexer ('\n' :        t) = Unix   : lexer t
 lexer (_    :        t) =          lexer t
 
-fuck [Unix, Unix] = Just '>'
-fuck [Unix, DOS ] = Just '<'
-fuck [Unix, Mac ] = Just '+'
-fuck [DOS,  Unix] = Just '-'
-fuck [DOS,  DOS ] = Just '.'
-fuck [DOS,  Mac ] = Just ','
-fuck [Mac,  Unix] = Just '['
-fuck [Mac,  DOS ] = Just ']'
-fuck _            = Nothing
+translations :: [([Ending], Char)]
+translations = [ ([Unix, Unix], '>')
+               , ([Unix, DOS ], '<')
+               , ([Unix, Mac ], '+')
+               , ([DOS,  Unix], '-')
+               , ([DOS,  DOS ], '.')
+               , ([DOS,  Mac ], ',')
+               , ([Mac,  Unix], '[')
+               , ([Mac,  DOS ], ']')
+               ]
+
+translations' :: [(Char, [Ending])]
+translations' = map swap translations
+
+fuck :: [Ending] -> Maybe Char
+fuck = flip lookup translations
+
+unfuck :: Char -> [Char]
+unfuck x = case x of '\r' -> []
+                     '\n' -> []
+                     _    -> maybe [x] (concatMap emit) (lookup x translations')
+
+emit :: Ending -> String
+emit Unix = "\n"
+emit Mac  = "\r"
+emit DOS  = "\r\n"
